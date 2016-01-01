a3836247ba6af82d79a706d3593b943ee682430b
Yogesh Darji <yogeshdarji@cometnet-10-21-45-220.utdallas.edu>
2015-12-31 21:16:23 -0600

    Added Compressed.java

---
+++ src/ArraysStrings/CompressedString.java
@@ -0,0 +1,47 @@
+package ArraysStrings;
+
+public class CompressedString {
+
+	public String compressed(String s)
+	{
+		//char c[]=new char[100];
+		int count=1;
+		String ans ="";
+		char last = s.charAt(0);
+		
+		
+		for(int i=1;i<s.length();i++)
+		{
+			
+			
+			if(s.charAt(i)==last)
+			{
+				
+				count++;
+			}
+			
+			else
+			{
+				ans = ans +"" +last + ""+count;
+				count = 1;
+				last = s.charAt(i);
+			}
+			
+		}
+		
+		
+		return ans+last+count;
+		    
+			
+		}
+	
+	public static void main(String args[])
+	{
+		CompressedString cs = new CompressedString();
+		
+		String result = cs.compressed("aabccccaaad");
+		
+		System.out.println(result);
+	}
+	
+}
